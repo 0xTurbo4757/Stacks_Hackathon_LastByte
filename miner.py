8d0a3f04f2929693b844ab3eab8026bbfa3f2d9c
ZKhan007 <23100049@lums.edu.pk>
2021-08-08 01:11:40 +0500

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,4 +1,57 @@
 # Miner Code Here
 # Zain
 # sarib gamer
-#Test
+import hashFunction
+import json
+
+blocknumber = 
+previousBlockHash = ""
+
+transactionSarib = ""
+
+
+
+
+# Creating A Block
+testNonce = 0
+blockdata = {}
+# We are creating a Genesis Block 
+
+if(blocknumber == 1):
+    blockdata = {
+                blocknumber : {
+                    "Previous Hash: ": "00000000",
+                    "Data: ": transactionSarib,
+                    "Nonce: ": testNonce
+                }
+    }
+else:
+# The Block Is Not a Genesis Block
+    blockdata = {
+                blocknumber : {
+                    "Previous Hash: ": previousBlockHash,
+                    "Data: ": transactionSarib,
+                    "Nonce: ": testNonce
+                }
+    }
+
+# Writing it to JSON
+
+# Serializing json
+json_object = json.dumps(blockdata, indent=4)
+# Writing to sample.json
+with open("BlockChain", "a") as outfile:
+    outfile.write(json_object)
+
+
+
+# Implementing Proof Of WORK
+# Finding CORRECT NONCE
+
+nonce = 1;
+
+
+
+
+
+
