8b6b14c87e763107e97c2101e1b4b5cf694e71a9
0xTurbo4757 <0xTurbo4757@users.noreply.github.com>
2021-08-08 02:10:58 +0500

    Dev

--- market.py
+++ market.py
@@ -18,11 +18,19 @@ class Market:
         # Market Variables here
         self.OrderBook = []  # has dictionaries at each index
 
+        self.TEST_BLOCK_CHAIN_TESTING_ONLY = '{"1":{"PreviousHash":"0x000000000000","Data":"1,2,5","Nonce":"852946123"},"2":{"PreviousHash":"0x004433221100","Data":"2,3,2","Nonce":"681861688"},"3":{"PreviousHash":"0x006516138131","Data":"3,4,6","Nonce":"694206900"},"4":{"PreviousHash":"0x006861831815","Data":"4,1,3","Nonce":"616840233"}}'
         self.BlockChain = []
+
+        #Testing ONLY
+        self.BlockChain = json.loads(self.TEST_BLOCK_CHAIN_TESTING_ONLY)
         # self.
+
+        #Stores list of Merchant Pkey, network Address that we have encountered
+        self.ExistingMerchantList = []
     # EndFunction
 
-    def Add_To_OrderBook(self, merchant_type, merchant_public_key, comodity_to_sell, comodity_price):
+    #OK
+    def Add_To_OrderBook(self, merchant_type, merchant_public_key, comodity_to_sell, comodity_price):   
 
         self.OrderBook.append(
             {
@@ -32,18 +40,6 @@ class Market:
                 "m_price": comodity_price
             }
         )
-    # EndFunction
-
-    #OK
-    def Remove_From_OrderBook(self, merchant_public_key):
-
-        index = self.Get_Index_of_merchant_in_OrderBook(merchant_public_key)
-
-        if (index != -1):
-            self.OrderBook.pop(index)
-        else:
-            print("MERCHANT NOT FOUND. THIS SHOULDNT HAVE HAPPENED!")
-        #EndIf
     # EndFunction
 
     #OK
@@ -64,24 +60,16 @@ class Market:
     #EndFunction
 
     #OK
-    def Print_OrderBook(self):
+    def Remove_From_OrderBook(self, merchant_public_key):
 
-        if (len(self.OrderBook) == 0):
-            print("The order book is empty")
-            return
-        # EndIf
+        index = self.Get_Index_of_merchant_in_OrderBook(merchant_public_key)
 
-        index = 1
-        for current_order in self.OrderBook:
-            print("\nMerchant {}".format(index))
-            print("Merchant Type : {}".format(current_order["m_type"]))
-            print("Merchant Key  : {}".format(current_order["m_pkey"]))
-            print("Merchant Item : {}".format(current_order["m_item"]))
-            print("Merchant Price: {}".format(current_order["m_price"]))
-
-            index += 1
-        # EndFor
-    #EndFunction
+        if (index != -1):
+            self.OrderBook.pop(index)
+        else:
+            print("MERCHANT NOT FOUND. THIS SHOULDNT HAVE HAPPENED!")
+        #EndIf
+    # EndFunction
 
     #OK
     # Checks OrderBook for potential Transactions
@@ -148,18 +136,55 @@ class Market:
         print(potential_TXNs)
     # EndFunction
 
-    # Iterates through Blockchain to get latest Balance
+    # Iterates through Blockchain to get latest Balance of the merchant
     def Get_Merchant_Current_Balance(self, merchant_public_key):
         pass
     # EndFunction
 
-    def Run(self):
+    #Send OrderBook to the Merchant requesting it
+    def Send_OrderBook_to_Merchant(self, merchant_public_key):
+        pass
+    #EndFunction
 
-        # Main loop
+    def Add_Merchant_to_Existing_List(self, merchant_public_key, merchant_)
+    
+    def RunMarket(self):
+
+        #Main loop
         while True:
             pass
-        # EndWhile
-    # EndFunction
+        #EndWhile
+    #EndFunction
+
+    #OK
+    def Print_OrderBook(self):
+
+        if (len(self.OrderBook) == 0):
+            print("The order book is empty")
+            return
+        # EndIf
+
+        index = 1
+        for current_order in self.OrderBook:
+            print("\nMerchant {}".format(index))
+            print("Merchant Type : {}".format(current_order["m_type"]))
+            print("Merchant Key  : {}".format(current_order["m_pkey"]))
+            print("Merchant Item : {}".format(current_order["m_item"]))
+            print("Merchant Price: {}".format(current_order["m_price"]))
+
+            index += 1
+        # EndFor
+    #EndFunction
+
+    #OK
+    def Print_BlockChain(self):
+
+        print('\n', end='')
+
+        print(json.dumps(self.BlockChain, indent=4, sort_keys=True))
+        
+        print('\n', end='')
+    #EndFunction
 # EndClass
 
 
@@ -172,12 +197,13 @@ def main():
     market.Add_To_OrderBook("Seller", 2, "Chair", 40)
     market.Add_To_OrderBook("Buyer", 3, "Fan", 30)
     market.Add_To_OrderBook("Seller", 4, "Fan", 40)
-    market.Print_OrderBook()
+    #market.Print_OrderBook()
     #market.Remove_From_OrderBook(4)
-    print("\n---------------------------")
-
-    market.Get_All_Potential_TXN_in_OrderBook()
+    #print("\n---------------------------")
+    #market.Get_All_Potential_TXN_in_OrderBook()
     #print("P Key: {}".format(market.Get_Index_of_merchant_in_OrderBook(1)))
+
+    market.Print_BlockChain()
 # EndMain
 
 
